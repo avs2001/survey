9e2856f94fb1aa566d5aa31686d1e8c9e35993bf
Ciprian Iuga <ciprian@kebormed.com>
2025-06-16 12:34:06 +0300

    docs: add single choice usage example

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {MultipleChoice} from './shared/controls/multiple-choice/multiple-choice';
+import { MultipleChoice } from './shared/controls/multiple-choice/multiple-choice';
+import { SingleChoice } from './shared/controls/single-choice/single-choice';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MultipleChoice],
+  imports: [RouterOutlet, MultipleChoice, SingleChoice],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
