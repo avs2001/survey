097188bf873eec78f2e78661f3133f3e59f1c0fb
Iuga Ciprian <ciprian@>
2025-06-16 11:31:01 +0300

    .

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MultipleChoice} from './shared/controls/multiple-choice/multiple-choice';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MultipleChoice],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
